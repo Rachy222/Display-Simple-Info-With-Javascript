196abef36140706295008b90381cbcb1b52fac07
Ezeh Racheal <chikaezeh77@gmail.com>
2022-06-22 12:34:21 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let surName="Ezeh";
+let firstName="Chika";
+let lastName="Racheal";
+let myHeight="6.8 Inches";
+let myCountry="USA";
+document.write("My Name is " + surName + " " + firstName + " "+ lastName +"." + "<br>"+ "My Height is " + myHeight +"." + "<br>" + "I' am proud to be from " + myCountry + " "+ "as my country " +".");
